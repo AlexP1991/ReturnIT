e4761dd755ee268254b4d6d2065c6987ac85f635
Alexandru Polli <137920824+AlexP1991@users.noreply.github.com>
2023-12-05 00:31:07 +0200

    eu

--- demo/src/main/java/com/example/demo/model/dtos/PersonDTO.java
+++ demo/src/main/java/com/example/demo/model/dtos/PersonDTO.java
@@ -3,6 +3,7 @@ package com.example.demo.model.dtos;
 public class PersonDTO {
     private String personName;
     //la fel ca in calasa Person
+    //test
     public String getPersonName() {
         return personName;
     }
